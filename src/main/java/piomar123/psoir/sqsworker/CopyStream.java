3eb72628066363d1693cf53f0ead9713d5cbb1e2
piomar123 <piomar123@gmail.com>
2017-01-30 22:23:16 +0100

    Square thumbnails. MIME type check on thumbnail creation.

--- src/main/java/piomar123/psoir/sqsworker/CopyStream.java
+++ src/main/java/piomar123/psoir/sqsworker/CopyStream.java
@@ -1,11 +1,14 @@
 package piomar123.psoir.sqsworker;
 
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * ByteArrayOutputStream convertible to InputStream without duplicated arrays.
+ * Efficient and unsafe ByteArrayOutputStream convertible to InputStream without duplicated arrays.
  * http://stackoverflow.com/a/36961688
  * Created by Piomar on 2017-01-30.
  */
@@ -15,11 +18,40 @@ public class CopyStream extends ByteArrayOutputStream {
     }
 
     /**
+     * Reads given stream using {@link #readStream(InputStream)}.
+     */
+    public CopyStream(InputStream stream) throws IOException {
+        readStream(stream);
+    }
+
+    /**
      * Get an input stream based on the contents of this output stream.
      * Do not use the output stream after calling this method.
      * @return an {@link InputStream}
      */
     public ByteArrayInputStream toInputStream() {
         return new ByteArrayInputStream(this.buf, 0, this.count);
+    }
+
+    /**
+     * Raw reference to buffer. Any changes to data will cause unexpected behaviour.
+     * @return direct reference to buffer
+     */
+    public byte[] referenceToBuffer() {
+        return this.buf;
+    }
+
+    /**
+     * Reads given stream.
+     * @param stream input stream
+     * @throws IOException propagates {@link InputStream#read(byte[], int, int)}
+     */
+    public void readStream(InputStream stream) throws IOException {
+        int nRead;
+        byte[] buffer = new byte[65536];
+
+        while ((nRead = stream.read(buffer, 0, buffer.length)) != -1) {
+            this.write(buffer, 0, nRead);
+        }
     }
 }
--- src/main/java/piomar123/psoir/sqsworker/SQSWorker.java
+++ src/main/java/piomar123/psoir/sqsworker/SQSWorker.java
@@ -9,11 +9,15 @@ import com.amazonaws.services.sqs.model.MessageAttributeValue;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 import piomar123.psoir.sqsworker.SimpleLogger.LogLevel;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.net.URLConnection;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -101,30 +105,36 @@ public class SQSWorker {
     }
 
     private void executeAction(S3Object source, String action, Map<String, MessageAttributeValue> attrs) throws IOException {
-        S3ObjectInputStream stream = source.getObjectContent();
+        CopyStream cs = new CopyStream(source.getObjectContent());
+        ByteArrayInputStream bais = cs.toInputStream();
+
         switch(action){
             case Actions.Thumbnail:
-                log.info("Creating thumbnail");
+                String mime = setCorrectMimeType(source, bais);
+
+                log.info(String.format("Creating thumbnail for %s", source.getKey()));
                 CopyStream outputStream = new CopyStream();
                 Thumbnails
-                        .of(stream)
+                        .of(bais)
+                        .crop(Positions.CENTER)
                         .size(config.THUMBS_SIZE.width, config.THUMBS_SIZE.height)
                         .useOriginalFormat()
                         .outputQuality(0.9)
                         .toOutputStream(outputStream);
-                ByteArrayInputStream input = outputStream.toInputStream();
+                ByteArrayInputStream inputStream = outputStream.toInputStream();
                 String[] keyParts = source.getKey().split("/");
                 String filename = keyParts[keyParts.length-1];
                 String s3destKey = config.S3_KEY_PREFIX_THUMBS + filename;
 
                 ObjectMetadata metadata = new ObjectMetadata();
-                metadata.setContentLength(input.available());
+                metadata.setContentLength(inputStream.available());
+                if(mime != null) metadata.setContentType(mime);
 
                 Map<String, String> thumbMetadata = new HashMap<>();
                 thumbMetadata.put("source-key", source.getKey());
                 metadata.setUserMetadata(thumbMetadata);
 
-                PutObjectRequest request = new PutObjectRequest(source.getBucketName(), s3destKey, input, metadata)
+                PutObjectRequest request = new PutObjectRequest(source.getBucketName(), s3destKey, inputStream, metadata)
                         .withCannedAcl(CannedAccessControlList.Private);
                 s3.putObject(request);
 
@@ -132,10 +142,32 @@ public class SQSWorker {
                 details.put("s3bucket", source.getBucketName());
                 details.put("s3srcKey", source.getKey());
                 details.put("s3destKey", s3destKey);
+                details.put("MIME", (mime != null) ? mime : "?");
                 simpleLog.log(LogLevel.info, "Created thumbnail", details);
 
                 break;
         }
+    }
+
+    private String setCorrectMimeType(S3Object object, ByteArrayInputStream bais) throws IOException {
+        String mime = URLConnection.guessContentTypeFromStream(bais);
+        bais.reset();
+        if (mime == null) {
+            log.info("Unknown MIME type");
+            return null;
+        }
+        log.info(String.format("Determined MIME: %s", mime));
+        ObjectMetadata metadata = object.getObjectMetadata();
+        metadata.setContentType(mime);
+        CopyObjectRequest copyRequest = new CopyObjectRequest(
+                object.getBucketName(),
+                object.getKey(),
+                object.getBucketName(),
+                object.getKey())
+            .withCannedAccessControlList(CannedAccessControlList.Private)
+            .withNewObjectMetadata(metadata);
+        s3.copyObject(copyRequest);
+        return mime;
     }
 
     public static final class Actions {
